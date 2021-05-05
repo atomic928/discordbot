f8fc0c92ee7b4428423029d49cda14bbc0206121
atomic928 <64305950+atomic928@users.noreply.github.com>
2021-05-05 21:14:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,22 +33,22 @@ async def on_message(message):
     
     data = []
     moji = "("
-    if len(dicea) <= 10:
-        for ndn in dicea: #複数の振り方に対応
-            dicem = re.search("\d+", ndn)
-            dicei = int(dicem.group())
-            ataim = re.search("d\d+", ndn)
-            ataii = int(ataim.group()[1:])
-            if dicei < 300:
-                if re.match("/(\d+d\d+\+)*\d+d\d+", reply):
+    if re.match("/(\d+d\d+\+)*\d+d\d+", reply):
+        if len(dicea) <= 10:
+            for ndn in dicea:#複数の振り方に対応
+                dicem = re.search("\d+", ndn)
+                dicei = int(dicem.group())
+                ataim = re.search("d\d+", ndn)
+                ataii = int(ataim.group()[1:])
+                if dicei < 300:
                     for first in range(dicei):
                         data.append(random.randrange(1,ataii+1,1))
-                        if first < dicei-1 or ndn != dicea[-1]:
-                            moji += str(data[first]) + " + "
-                        else:
-                            moji += str(data[first])
-            else:
-                return
+
+    for i in range(len(data)):
+        if i != len(data)-1:
+            moji += str(data[i]) + " + "
+        else:
+            moji += str(data[i])
     moji += ")"
     await message.channel.send(str(sum(data))+ " " +moji)
         
